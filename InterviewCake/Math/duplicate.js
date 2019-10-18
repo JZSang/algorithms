c3a19ef5f69c9f708083f1ae01c3693eabd21cfb
Jason Sang <dotsangjason@gmail.com>
2019-10-18 17:21:49 -0400

    and that's interview cake folks

---
+++ InterviewCake/Math/duplicate.js
@@ -0,0 +1,63 @@
+function findRepeat(numbers) {
+
+  let total = 0;
+  for (let c of numbers) {
+    total += c;
+  }
+  let actualTotal = 0;
+  for (let i = 1; i <= numbers.length-1; i++) {
+    actualTotal += i;
+  }
+  return total - actualTotal;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Tests
+
+let desc = 'short array';
+let actual = findRepeat([1, 2, 1]);
+let expected = 1;
+assertEqual(actual, expected, desc);
+
+desc = 'medium array';
+actual = findRepeat([4, 1, 3, 4, 2]);
+expected = 4;
+assertEqual(actual, expected, desc);
+
+desc = 'long array';
+actual = findRepeat([1, 5, 9, 7, 2, 6, 3, 8, 2, 4]);
+expected = 2;
+assertEqual(actual, expected, desc);
+
+function assertEqual(a, b, desc) {
+  if (a === b) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL: ${a} != ${b}`)
+  }
+}
+
+function assertThrowsError(func, desc) {
+  try {
+    func();
+    console.log(`${desc} ... FAIL`);
+  } catch (e) {
+    console.log(`${desc} ... PASS`);
+  }
+}
