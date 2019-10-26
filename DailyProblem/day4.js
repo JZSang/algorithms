b49f249ba300dd10186e27402dff0d1a8ba7830b
Jason Sang <dotsangjason@gmail.com>
2019-10-25 22:04:12 -0400

    code rust

--- DailyProblem/day4.js
+++ DailyProblem/day4.js
@@ -6,3 +6,34 @@ Given an array of integers, find the first missing positive integer in linear ti
 For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0] should give 3.
 You can modify the input array in-place.
 */
+
+function missing(array) {
+    let foundOne = false;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === 1) {
+            foundOne = true;
+        }
+    }
+    if (!foundOne) return 1;
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] <= 0) {
+            array[i] = 1;
+        }
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        if (Math.abs(array[i]) <= array.length && array[Math.abs(array[i])-1]> 0) {
+            array[Math.abs(array[i])-1] = -array[Math.abs(array[i])-1];
+        }
+    }
+    let count = 0;
+    let i = 0;
+    while (array[i] < 0) {
+        count++;
+        i++;
+    } 
+    return count+1;
+}
+let ans = [1,8,2,7,8,4,6,4,2,5,3]
+console.log(missing(ans))
