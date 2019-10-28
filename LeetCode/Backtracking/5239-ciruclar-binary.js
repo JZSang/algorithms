3f25b37ec2a960ba7bc6e052e58c3ae107477f1c
Jason Sang <dotsangjason@gmail.com>
2019-10-28 19:11:24 -0400

    wish

--- LeetCode/Backtracking/5239-ciruclar-binary.js
+++ LeetCode/Backtracking/5239-ciruclar-binary.js
@@ -1,30 +1,5 @@
 var circularPermutation = function(n, start) {
-    let set = new Set([start]);
-    let finish = []
-    let ans = [start];
-    function backtrack(num) {
-        if (ans.length >= 2 ** n) {
-            for (let i = 0; i < n; i++) {
-                if (num ^ (1 << i) === ans[0]) {
-                    finish = ans.slice();
-                }
-                
-            }
-            return;
-        }
-        for (let i = 0; i < n; i++) {
-            let prod = num ^ (1 << i);
-            if (set.has(prod)) continue;
-            set.add(prod);
-            ans.push(prod);
-            backtrack(prod);
-            ans.pop();
-            set.delete(prod);
-            if (finish.length) return;
-            
-        }
-    }
-    backtrack(start);
+    
     return finish;
 };
 
---
+++ personal/interview_questions/relationships.js
@@ -0,0 +1,49 @@
+let relationships = [
+    ["Bart", "brother", "Lisa"],
+    ["Bart", "son", "Homer"],
+    ["Marge", "wife", "Homer"],
+    ["Lisa", "daughter", "Homer"],
+    ["Bart", "brother", "Nam"],
+    ["Nam", "sister", "Lisa"]
+]
+
+function solution(relationships, name1, name2) {
+
+    let relationship = {};
+    let graph = {};
+    let visited = new Set();
+
+    for (let c of relationships) {
+        graph[c[0]] = graph[c[0]] || [];
+        graph[c[0]].push(c[2]);
+        relationship[c[0] + c[2]] = c[1];
+    }
+    // check if name1 or name2 doesn't exist
+
+    let ans = [];
+
+    function dfs(from, array) {
+        array.push(from);
+        if (from === name2) {
+            ans.push(array.join(" "));
+            array.pop();
+            return;
+        }
+        visited.add(from);
+        for (let c of graph[from]) {
+            if (visited.has(c)) continue;
+            array.push(relationship[from + c]) 
+            dfs(c, array);
+            array.pop();
+        }
+        visited.delete(from);
+        array.pop();
+    }
+
+    dfs(name1, []);
+    return ans;
+}
+
+console.log(solution(relationships, "Bart", "Homer"))
+
+// ["Bart son Homer", "Bart brother Lisa daughter Homer"]
